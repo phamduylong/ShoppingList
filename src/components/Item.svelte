adf08bcf2f43ee232a13e74d6770d4981864266a
longph <longphamduy2002@gmail.com>
2023-01-11 00:20:38 +0200

    moved link tags to appropriate file

--- src/components/Item.svelte
+++ src/components/Item.svelte
@@ -2,6 +2,16 @@
 	export let item = { item: '', quantity: 1, note: '' };
 </script>
 
+
+
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css" />
+<link
+	rel="stylesheet"
+	href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+/>
+
+
+
 <div class="item-container">
 	{item.item} x {item.quantity}<br />
 	{#if item.note !== ''}<strong>Note: </strong>{item.note}<br />{/if}
