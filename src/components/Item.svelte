7f80be669544d11d0d7bd8ea8efd1ff141306f45
longph <longphamduy2002@gmail.com>
2023-01-10 18:37:36 +0200

    move item to a single component

---
+++ src/components/Item.svelte
@@ -0,0 +1,32 @@
+<script>
+	export let item = { item: '', quantity: 1, note: '' };
+</script>
+
+<div class="item-container">
+	{item.item} x {item.quantity}<br />
+	{#if item.note !== ''}<strong>Note: </strong>{item.note}<br />{/if}
+	<button on:click><i class="fa fa-trash" /></button>
+</div>
+
+<style>
+	.item-container {
+		position: relative;
+		display: inline-block;
+		height: 2%;
+		width: 100%;
+		background-color: orange;
+		border-radius: 5px;
+		border: black solid 1.5px;
+		margin: 5px;
+		padding: 1em;
+	}
+
+	button {
+		position: absolute;
+		top: 50%;
+		right: 1%;
+		background-color: orange;
+		border: none;
+		margin-left: auto;
+	}
+</style>
--- src/components/ShoppingList.svelte
+++ src/components/ShoppingList.svelte
@@ -1,11 +1,11 @@
 <script>
     import { createEventDispatcher } from 'svelte';
-
+	import Item from './Item.svelte';
     let dispatch = createEventDispatcher();
 
     export let shopping_list = [];
 
-    function emitFinish(index) {
+    function emitDeleteItem(index) {
         dispatch('deleteitem', index);
     }
 </script>
@@ -13,17 +13,7 @@
 <div id="shopping_list">
 	<h2><b>Shopping List</b></h2>
 	{#each shopping_list as item, index}
-		{#if item.note === ''}
-			<div class="item-container">
-				{item.item} x {item.quantity}<br />
-				<button on:click={() => emitFinish(index)}><i class="fa fa-trash" /></button>
-			</div>
-		{:else}
-			<div class="item-container">
-				{item.item} x {item.quantity}<br /><strong>Note: </strong>{item.note}<br />
-				<button on:click={() => emitFinish(index)}><i class="fa fa-trash" /></button>
-			</div>
-		{/if}
+		<Item {item} on:click={() => emitDeleteItem(index)}/>
 	{/each}
 </div>
 
@@ -35,26 +25,6 @@
 		transform: translate(-50%, -50%);
 	}
 
-	.item-container {
-		position: relative;
-		display: inline-block;
-		height: 2%;
-		width: 100%;
-		background-color: orange;
-		border-radius: 5px;
-		border: black solid 1.5px;
-		margin: 5px;
-		padding: 1em;
-	}
-
-	button {
-		position: absolute;
-		top: 50%;
-		right: 1%;
-		background-color: orange;
-		border: none;
-		margin-left: auto;
-	}
 
 	#shopping_list {
 		display: flex;
