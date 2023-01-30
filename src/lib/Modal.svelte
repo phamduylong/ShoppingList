0a61d71702a9557da5639df25ee2a934ace875a6
longph <longphamduy2002@gmail.com>
2023-01-30 23:20:12 +0200

    changed layout to popup modal

--- src/lib/Modal.svelte
+++ src/lib/Modal.svelte
@@ -1,66 +1,72 @@
 <script>
-    import { closeModal } from 'svelte-modals'
-    import { modalResult } from './stores.js';
-    export let isOpen;
-  
-    export let title;
-    export let message;
-  
-  </script>
-  
-  {#if isOpen}
-    <div role="dialog" class="modal">
-      <div class="contents">
-        <h2>{title}</h2>
-        <p>{message}</p>
-        <div class="actions">
-            <button on:click={() => { modalResult.setTrue(); closeModal(); }}>Yes</button>
-          <button on:click={closeModal}>No</button>
-        </div>
-      </div>
-    </div>
-  {/if}
-  
-  <style>
-    .modal {
-      position: fixed;
-      top: 0;
-      bottom: 0;
-      right: 0;
-      left: 0;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      z-index: 10;
-      /* allow click-through to backdrop */
-      pointer-events: none;
-    }
-  
-    .contents {
-      min-width: 240px;
-      border-radius: 6px;
-      padding: 16px;
-      background: white;
-      display: flex;
-      flex-direction: column;
-      justify-content: space-between;
-      pointer-events: auto;
-    }
-  
-    h2 {
-      text-align: center;
-      font-size: 24px;
-    }
-  
-    p {
-      text-align: center;
-      margin-top: 16px;
-    }
-  
-    .actions {
-      margin-top: 32px;
-      display: flex;
-      justify-content: space-between;
-    }
-  
-  </style>
+	import { createEventDispatcher } from 'svelte';
+	import { modalResult, modalOpen } from './stores';
+	export let title;
+	export let message;
+  const dispatch = createEventDispatcher();
+</script>
+
+{#if $modalOpen}
+	<div role="dialog" class="modal">
+		<div class="contents">
+			<h2>{title}</h2>
+			<p>{message}</p>
+			<div class="actions">
+				<button
+					on:click={() => {
+            			dispatch('modalconfirmed');
+						modalOpen.close();
+					}}>Yes</button
+				>
+				<button
+					on:click={() => {
+						modalOpen.close();
+					}}>No</button
+				>
+			</div>
+		</div>
+	</div>
+{/if}
+
+<style>
+	.modal {
+		position: fixed;
+		top: 0;
+		bottom: 0;
+		right: 0;
+		left: 0;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		z-index: 10;
+		/* allow click-through to backdrop */
+		pointer-events: none;
+	}
+
+	.contents {
+		min-width: 240px;
+		border-radius: 6px;
+		padding: 16px;
+		background: white;
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+		pointer-events: auto;
+	}
+
+	h2 {
+		text-align: center;
+		font-size: 24px;
+	}
+
+	p {
+		text-align: center;
+		margin-top: 16px;
+	}
+
+	.actions {
+		margin-top: 32px;
+		display: flex;
+		justify-content: space-between;
+	}
+</style>
