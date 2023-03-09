856a100052cee4b66f55d1451a66cab8e24bd774
longph <longphamduy2002@gmail.com>
2023-03-09 22:16:40 +0200

    disabled button styling

--- src/lib/AddForm.svelte
+++ src/lib/AddForm.svelte
@@ -1,15 +1,17 @@
 <script>
-	import { createEventDispatcher } from "svelte";
+	import { createEventDispatcher } from 'svelte';
 	let dispatch = createEventDispatcher();
 	let item = '',
 		note = '',
 		quantity = 1;
-	$: valid = item !== "" && item !== undefined && !isNaN(quantity) && quantity > 0;
+	$: valid = item !== '' && !isNaN(Number(quantity)) && quantity > 0;
 
 	function emitAddItem() {
 		const new_item = {
-			item, quantity, note
-		}
+			item,
+			quantity,
+			note
+		};
 
 		dispatch('additem', new_item);
 		item = '';
@@ -24,10 +26,8 @@
 	href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
 />
 
-
-	
 <form on:submit|preventDefault={emitAddItem}>
-    <h2><b>Add New Item</b></h2>
+	<h2><b>Add New Item</b></h2>
 	<label for="item" class="required">Item</label>
 	<input type="text" name="item" placeholder="Item" required bind:value={item} />
 	<label for="quantity" class="required">Quantity</label><br />
@@ -42,7 +42,9 @@
 	/>
 	<label for="note">Note</label>
 	<input type="text" name="note" placeholder="Note for this item" bind:value={note} />
+
 	<button type="submit" disabled={!valid}>Add Item</button>
+
 </form>
 
 <style>
@@ -51,8 +53,8 @@
 		left: 50%;
 		top: -25%;
 		transform: translate(-50%, -50%);
-        margin-bottom: 3vw;
-		font-family: "Courier New", sans-serif;
+		margin-bottom: 3vw;
+		font-family: 'Courier New', sans-serif;
 		font-size: 2vw;
 	}
 
@@ -74,9 +76,9 @@
 	}
 
 	.required:after {
-		content:"*";
+		content: '*';
 		color: red;
-  	}
+	}
 
 	input {
 		border-top: none;
@@ -115,8 +117,13 @@
 		transform: translate(-50%, -50%);
 	}
 
-	@media only screen and (orientation: portrait) and (max-width: 600px) {
+	button:disabled,
+	button[disabled] {
+		background-color: #cccccc;
+		color: #666666;
+	}
 
+	@media only screen and (orientation: portrait) and (max-width: 600px) {
 		label {
 			font-size: 2vw;
 		}
